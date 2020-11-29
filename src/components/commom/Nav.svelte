a9a86fc7d08de51ba604adba64a7f94cc7f8ee9d
Alban Rodriguez <alban.rodriguez95@gmail.com>
2020-11-29 14:36:14 +0100

    feat: Creation of the main componenets & styles of the theme "Header + footer + mian css" and create hero of home

---
+++ src/components/commom/Nav.svelte
@@ -0,0 +1,88 @@
+<script>
+    import GithubIcon from "../svg/GithubIcon.svelte";
+    export let segment;
+</script>
+
+<style>
+    a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+    }
+
+    .header {
+        position: sticky;
+        top: 0;
+        font-weight: 300;
+        background: #e4fcfc;
+        background: rgb(228 252 252 / 47%);
+        backdrop-filter: blur(6px);
+        font-size: 0.95rem;
+    }
+
+    .wrapper--xl {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 60px;
+    }
+
+    .brand-logo {
+        width: 40px;
+        border-radius: 14px;
+    }
+
+    .navbar {
+        display: flex;
+        align-items: center;
+    }
+    .navbar-button {
+        display: none;
+        padding: 0.7rem 1rem;
+        font-size: 1rem;
+        margin-left: 1rem;
+    }
+    @media (min-width: 690px) {
+        .navbar-button {
+            display: inherit;
+        }
+    }
+
+    /*[aria-current] {*/
+    /*    position: relative;*/
+    /*    display: inline-block;*/
+    /*}*/
+
+    /*[aria-current]::after {*/
+    /*    position: absolute;*/
+    /*    content: '';*/
+    /*    width: calc(100% - 1em);*/
+    /*    height: 2px;*/
+    /*    display: block;*/
+    /*    bottom: -1px;*/
+    /*}*/
+
+</style>
+
+<header class="header">
+    <div class="wrapper--xl">
+        <div class="brand">
+            <a aria-current="{segment === undefined ? 'page' : undefined}" href="../../..">
+                <img src="logo.jpg" alt="Brand" class="brand-logo">
+            </a>
+        </div>
+        <nav class="navbar">
+            <a href="">
+                View on Github <GithubIcon />
+            </a>
+            <a aria-current="{segment === undefined ? 'page' : undefined}" href="../../..">
+                <button class="navbar-button bg-primary">Join the community</button>
+            </a>
+        </nav>
+        <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+             the blog data when we hover over the link or tap it on a touchscreen -->
+        <!--		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>-->
+    </div>
+</header>
+
+
---
+++ src/components/home/Hero.svelte
@@ -0,0 +1,64 @@
+<style>
+    .hero {
+        background: var(--background-color);
+        background-image: url(https://www.vibely.io/img/ico-dots.svg), url(https://www.vibely.io/img/ico-dots.svg), url(https://www.vibely.io/img/bgImg-noodleLightBlue.svg), url(https://d3lqi7z5du42f4.cloudfront.net/assets/shapes/bgImg-rightNoodle.svg), url(https://d3lqi7z5du42f4.cloudfront.net/assets/shapes/bgImg-doubleNoodle.svg), url(https://www.vibely.io/img/bgDiag.svg);
+        background-repeat: no-repeat;
+        background-size: 8%, 8%, 30%, 12%, 24%, contain;
+        background-position: 60% 70px, 90% bottom, 0 320px, right 140px, right 520px, 0 bottom;
+        padding-bottom: 2rem;
+        min-height: 750px;
+    }
+
+    .information {
+        flex: 1 0 60%;
+    }
+
+    .video {
+        flex: 1 0 40%;
+    }
+
+    .squiggly {
+        color: var(--primary-color);
+        background-image: url(https://d3lqi7z5du42f4.cloudfront.net/assets/shapes/squigglyGreen2.svg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center bottom
+    }
+
+    .video-shape {
+        background: white;
+        width: 320px;
+        height: 600px;
+        border-radius: 18px;
+    }
+
+    .caption {
+        background-image: url(https://d3lqi7z5du42f4.cloudfront.net/assets/shapes/gra-arrow.svg);
+        background-repeat: no-repeat;
+        padding: 2rem 0 0 0;
+        background-position: 50% 0px;
+    }
+    @media (min-width: 590px) {
+        .caption {
+            padding: 2.5rem 3rem 0 43%;
+            background-position: 356px 13px;
+        }
+    }
+</style>
+
+<section class="hero">
+    <div class="wrapper flex-wrap">
+        <div class="information">
+            <h1>Domain-Driven Design <span class="squiggly">#typescript</span> community</h1>
+            <p><span class="text-bold-secondary">Challenges</span> 👨🏼‍💻 with <span class="text-bold-primary">DDD</span>,
+                <span class="text-bold-primary">code skeletons, tools, best practices, , and lasting friendships Warning</span>: extreme
+                engagement & loyalty may occur.</p>
+            <button class="bg-primary">Join Community</button>
+            <button class="bg-base">Read more now..</button>
+            <p class="caption">Discover the main contents, events and reasons to join the community. It's free.</p>
+        </div>
+        <div class="video">
+            <div class="video-shape"></div>
+        </div>
+    </div>
+</section>
