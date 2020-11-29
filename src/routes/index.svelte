38df8dec1131cd17add694a1a10127da95b020e8
Alban Rodriguez <alban.rodriguez95@gmail.com>
2020-11-29 16:56:53 +0100

    feat: Add Metadata component and create index metadata

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,15 @@
 <script>
     import Hero from "../components/home/Hero.svelte";
+    import MetaData from "../components/commom/MetaData.svelte";
+
+    const metadata = {
+        title: 'Domain-Driven Design #typescript community',
+        description: 'Discover the main contents, events and reasons to join the community. It"s free.',
+        image: 'https://domain-driven-design-typescript/shared_image.jpg',
+        imageAlt: 'Domain-Driven Design typescript community',
+        url: 'domain-driven-design-typescript.com'
+    }
 </script>
 
+<MetaData {metadata}/>
 <Hero />
